e633bad835b4f55715a777a9e25b3d3c7e571ce8
Ross Willett <rwillett@lyndendoor.com>
2017-07-06 23:46:16 -0700

    Allow for custom characters and classes
    for example remove the text-padding: 10 and use a font-awesome <i> tag:
    $.ScrollUpToTop({
        "scrollText": '<i class="fa fa-arrow-circle-o-up"></i>',
        "scrollClass": "",
    });

--- src/js/jquery.scrolluptotop.js
+++ src/js/jquery.scrolluptotop.js
@@ -1,20 +1,22 @@
 (function($){
-    'use strcit';
-    
-    $.fn.ScrollUpToTop = function(options){
-        
+    'use strict';
+
+    $.ScrollUpToTop = function(options){
+
         // Default options
         options = $.extend({
-                "speedUp": 800
+                "speedUp": 800,
+                "scrollText": "^",
+                "scrollClass": "scrollToTopDef",
             }, options);
-        
+
         // Variables
-        var $btnScroll = $("<button id=\"ScrollUpToTop\">^</button>").hide();
+        var $btnScroll = $("<button id=\"ScrollUpToTop\" class=\""+options.scrollClass+"\">"+options.scrollText+"</button>").hide();
         var $body = $(document.body);
-        
+
         // insert scroll up to top button
         $($body).append($btnScroll);
-        
+
         // attach click event on button
         $($btnScroll).click(function(e){
             e.preventDefault();
@@ -23,7 +25,7 @@
                 options.speedUp
             );
         });
-        
+
         $(window).scroll(function(){
             if( $(window).scrollTop() > 0) {
                 $($btnScroll).fadeIn();
@@ -31,7 +33,7 @@
                 $($btnScroll).fadeOut();
             }
         });
-        
+
     };
-        
+
 })(jQuery);
